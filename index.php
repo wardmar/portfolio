10a5332babc882c669c9182c524fd4fc3d1522f6
wardmar <alexis.wardmar@gmail.com>
2018-06-13 17:24:24 +0200

    header

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/logoblason.png" />
+    <title>Portfolio</title>
+</head>
+<body>
+    <header>
+        <img class="logoinit" src='img/logoblason.png' alt='logo'/>
+        <h1 id="titre">Portfolio</h1>
+        <a class="connect">Connect</a>
+    </header>
+    <div class="menu">
+        <img class="deroul" src="img/angle-down.svg" alt="">
+    </div>
+
+
+
+</body>
+</html>
